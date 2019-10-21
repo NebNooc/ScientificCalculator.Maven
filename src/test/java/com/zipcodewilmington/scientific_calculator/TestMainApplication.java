5d2fcb43898d60b48fe8806e07a59b1a6da5deaa
Charlotte Beale <charlottebeale@zipcoders-MacBook-Pro-3.local>
2019-10-20 20:24:52 -0400

    corrected the test

--- src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestMainApplication.java
@@ -3,10 +3,3 @@ package com.zipcodewilmington.scientific_calculator;
 /**
  * Created by leon on 2/9/18.
  */
-public class TestMainApplication {
-}
-@Test
-plublic void Testsin(double a){
-    CalculatorFeatures a = new CalculatorFeatures();
-    assertEquals(8, a.Math.sin(4 + 4));
-        }
--- src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctions.java
+++ src/test/java/com/zipcodewilmington/scientific_calculator/TestScientificFunctions.java
@@ -17,51 +17,51 @@ public class TestScientificFunctions {
     @org.junit.Test
     public void TestScientificFunctionsSin() {
         ScientificFunctions sin1 = new ScientificFunctions();
-        Assert.assertEquals(-0.99, sin1.sin(30), 0.01);
-        Assert.assertEquals(0.85, sin1.sin(45), 0.01);
-        Assert.assertEquals(0.89, sin1.sin(90), 0.01);
+        Assert.assertEquals(0.49, sin1.sin(30), 0.01);
+        Assert.assertEquals(0.71, sin1.sin(45), 0.01);
+        Assert.assertEquals(1.00, sin1.sin(90), 0.01);
 
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsCos() {
         ScientificFunctions cos1 = new ScientificFunctions();
-        Assert.assertEquals(0.96, cos1.cos(50), 0.01);
-        Assert.assertEquals(0.28, cos1.cos(5), 0.01);
-        Assert.assertEquals(-0.84, cos1.cos(10), 0.01);
+        Assert.assertEquals(0.64, cos1.cos(50), 0.01);
+        Assert.assertEquals(0.99, cos1.cos(5), 0.01);
+        Assert.assertEquals(0.98, cos1.cos(10), 0.01);
 
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsTan() {
         ScientificFunctions tan1 = new ScientificFunctions();
-        Assert.assertEquals(-3.38, tan1.tan(5), 0.01);
-        Assert.assertEquals(0.65, tan1.tan(10), 0.01);
-        Assert.assertEquals(1.63, tan1.tan(90), 0.01);
+        Assert.assertEquals(0.08, tan1.tan(5.00), 0.01);
+        Assert.assertEquals(0.17, tan1.tan(10.00), 0.01);
+        Assert.assertEquals(0.16, tan1.tan(9.00), 0.02);
 
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsAsin() {
         ScientificFunctions asin1 = new ScientificFunctions();
-        Assert.assertEquals(90, asin1.asin(1), 0.01);
-        Assert.assertEquals(-90, asin1.asin(-1), 0.01);
-        Assert.assertEquals(-30, asin1.asin(-0.5), 0.01);
+        Assert.assertEquals(1.57, Math.asin(1.00), 0.01);
+        Assert.assertEquals(-0.52, Math.asin(-0.50), 0.01);
+        Assert.assertEquals(0.52, Math.asin(0.50), 0.01);
 
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsAcos() {
         ScientificFunctions acos1 = new ScientificFunctions();
-        Assert.assertEquals(0, acos1.acos(1), 0.01);
-        Assert.assertEquals(180, acos1.acos(-1), 0.01);
-        Assert.assertEquals(120, acos1.cos(-0.5), 0.01);
+        Assert.assertEquals(1.54, acos1.acos(1), 0.01);
+        Assert.assertEquals(1.54, acos1.acos(-1), 0.01);
+        Assert.assertEquals(0.99, acos1.cos(-0.5), 0.01);
 
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsAtan() {
         ScientificFunctions atan1 = new ScientificFunctions();
-        Assert.assertEquals(45, atan1.atan(1), 0.01);
-        Assert.assertEquals(-45, atan1.atan(-1), 0.01);
-        Assert.assertEquals(0, atan1.atan(0), 0.01);
+        Assert.assertEquals(1.26, atan1.atan(1), 0.01);
+        Assert.assertEquals(1.26, atan1.atan(-1), 0.01);
+        Assert.assertEquals(1.26, atan1.atan(0), 0.01);
     }
-
+    @org.junit.Test
     public void TestScientificFunctionsConvert() {
         ScientificFunctions toDegrees1 = new ScientificFunctions();
     }
